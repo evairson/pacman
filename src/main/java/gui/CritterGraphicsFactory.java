95617f5b04f3064475478fd2f3947b326b777d23
Lùkas K <lukas.koltes@hotmail.fr>
2023-10-16 22:03:55 +0200

    - updates sprites

--- src/main/java/gui/CritterGraphicsFactory.java
+++ src/main/java/gui/CritterGraphicsFactory.java
@@ -30,10 +30,10 @@ public final class CritterGraphicsFactory {
         var size = 0.5; // facteur d'echelle de l'image
         var url = (critter instanceof PacMan) ? "pacman.png" :
                 switch ((Ghost) critter) {
-                    case BLINKY -> "blinky.png";
-                    case CLYDE -> "clyde.png";
-                    case INKY -> "inky.png";
-                    case PINKY -> "pinky.png";
+                    case BLINKY -> "ghost_blinky.png";
+                    case CLYDE -> "ghost_clyde.png";
+                    case INKY -> "ghost_inky.png";
+                    case PINKY -> "ghost_pinky.png";
                 };
         // chargement de l'image à partir du fichier url
         var image = new ImageView(new Image(url, scale * size, scale * size, true, true));
