ded87b87c68c948beab585b92211796fe5f16bd4
evairson <eva.herson@me.com>
2023-10-27 12:26:18 +0200

    Ajout de src/test/java et mise en place des premiers tests

--- src/main/java/gui/CritterGraphicsFactoryTest.java
+++
@@ -1,8 +0,0 @@
-package gui;
-
-public class CritterGraphicsFactoryTest {
-     
-    public void test(){
-        
-    }
-}
---
+++ src/test/java/CritterGraphicsFactoryTest.java
@@ -0,0 +1,12 @@
+package gui;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CritterGraphicsFactoryTest {
+    
+    @Test
+    public void test(){
+        System.out.println("voila");
+    }
+}
