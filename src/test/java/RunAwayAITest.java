5d05873e93278a3e482ddec83b80d1ff83bd4e51
Moetez Bedoui <moetez.bedoui@etu.u-paris.fr>
2023-11-10 00:19:56 +0100

    Création de la méthode de test pour la méthode GetDirection

--- src/test/java/RunAwayAITest.java
+++ src/test/java/RunAwayAITest.java
@@ -25,11 +25,21 @@ public class RunAwayAITest {
 
         ArrayList<IntCoordinates> result = RunAwayAI.voisins_plus_proches_des_plus_loins(pacmanPos, ghostPos, config);
 
-        // Ajoutez vos assertions ici en fonction des résultats attendus
-        // Par exemple :
+        
         assertEquals(3, result.size());
         assertTrue(result.contains(new IntCoordinates(2, 3)));
         assertTrue(result.contains(new IntCoordinates(3, 2)));
         assertTrue(result.contains(new IntCoordinates(3, 3)));
+    }
+
+    @Test
+    public void testGetDirection() {
+        MazeConfig config = makeExample1();
+        IntCoordinates pacmanPos = config.pacManPos;
+        IntCoordinates ghostPos = config.blinkyPos;
+
+        Direction result = RunAwayAI.getDirection(config, pacmanPos, ghostPos);
+
+        assertEquals(Direction.UP, result);
     }
 }
