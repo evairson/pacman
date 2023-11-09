d33a32a354f38facd96901761e779f44dc97aed7
igor <igor2004444@gmail.com>
2023-11-10 00:20:05 +0100

    début test

---
+++ src/test/java/GhostTest.java
@@ -0,0 +1,13 @@
+import GhostsAI.BlinkyAI;
+import GhostsAI.InkyAI;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import model.Ghost;
+public class GhostTest{
+    @Test
+    public void goToHomeTest(){
+
+
+    }
+
+}
