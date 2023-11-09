74b7f7cba348641f7ea925f8fd9698a79094d466
Moetez Bedoui <moetez.bedoui@etu.u-paris.fr>
2023-11-10 00:18:01 +0100

    Création de la méthode de test pour la méthode Voisins_plus_proches_des_plus_loins

--- src/test/java/RunAwayAITest.java
+++ src/test/java/RunAwayAITest.java
@@ -17,4 +17,19 @@ public class RunAwayAITest {
         assertTrue(result.contains(new IntCoordinates(2, 3)));
         assertTrue(result.contains(new IntCoordinates(3, 2)));
     }
+    
+    public void testVoisinsPlusProchesDesPlusLoins() {
+        MazeConfig config = makeExample1();
+        IntCoordinates pacmanPos = config.pacManPos;
+        IntCoordinates ghostPos = config.blinkyPos;
+
+        ArrayList<IntCoordinates> result = RunAwayAI.voisins_plus_proches_des_plus_loins(pacmanPos, ghostPos, config);
+
+        // Ajoutez vos assertions ici en fonction des résultats attendus
+        // Par exemple :
+        assertEquals(3, result.size());
+        assertTrue(result.contains(new IntCoordinates(2, 3)));
+        assertTrue(result.contains(new IntCoordinates(3, 2)));
+        assertTrue(result.contains(new IntCoordinates(3, 3)));
+    }
 }
