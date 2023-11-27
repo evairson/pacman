d69af666635adb37852d65fefb0f8541be5cb248
evairson <eva.herson@me.com>
2023-11-27 11:07:51 +0100

    #53 fix merge

--- src/main/java/gui/AnimationController.java
+++ src/main/java/gui/AnimationController.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
